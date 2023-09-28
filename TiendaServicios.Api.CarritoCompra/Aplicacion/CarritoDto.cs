b1d1ae45431be8ba2c9d932c50110e0372dd962f
Cristián Lara <cristian.lara.n@gmail.com>
2023-09-28 16:36:41 -0300

    Agregar archivos de proyecto.

---
+++ TiendaServicios.Api.CarritoCompra/Aplicacion/CarritoDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TiendaServicios.Api.CarritoCompra.Aplicacion
+{
+    public class CarritoDto
+    {
+        public int CarritoId { get; set; }
+        public DateTime? FechaCreacionSesion { get; set; }
+        public IEnumerable<CarritoDetalleDto> ListaProductos { get; set; }
+    }
+}
---
+++ TiendaServicios.Api.Libro.Tests/LibrosServiceTest.cs
@@ -0,0 +1,113 @@
+﻿using AutoMapper;
+using GenFu;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TiendaServicios.Api.Libro.Aplicacion;
+using TiendaServicios.Api.Libro.Modelo;
+using TiendaServicios.Api.Libro.Persistencia;
+using Xunit;
+
+namespace TiendaServicios.Api.Libro.Tests
+{
+    public class LibrosServiceTest
+    {
+        private IEnumerable<LibreriaMaterial> ObtenerDataPrueba() 
+        {
+            A.Configure<LibreriaMaterial>().Fill(x => x.Titulo).AsArticleTitle()
+                .Fill(x => x.LibreriaMaterialId, () => { return Guid.NewGuid(); });
+
+            var lista = A.ListOf<LibreriaMaterial>(30);
+            lista[0].LibreriaMaterialId = Guid.Empty;
+
+            return lista;
+        }
+
+
+        private Mock<ContextoLibreria> CrearContexto() 
+        {
+            var datarPrueba = ObtenerDataPrueba().AsQueryable();
+
+            var dbSet = new Mock<DbSet<LibreriaMaterial>>();
+            dbSet.As<IQueryable<LibreriaMaterial>>().Setup(x => x.Provider).Returns(datarPrueba.Provider);
+            dbSet.As<IQueryable<LibreriaMaterial>>().Setup(x => x.Expression).Returns(datarPrueba.Expression);
+            dbSet.As<IQueryable<LibreriaMaterial>>().Setup(x => x.ElementType).Returns(datarPrueba.ElementType);
+            dbSet.As<IQueryable<LibreriaMaterial>>().Setup(x => x.GetEnumerator()).Returns(datarPrueba.GetEnumerator());
+
+            dbSet.As<IAsyncEnumerable<LibreriaMaterial>>().Setup(x => x.GetAsyncEnumerator(new System.Threading.CancellationToken()))
+                .Returns(new AsyncEnumerator<LibreriaMaterial>(datarPrueba.GetEnumerator()));
+
+            dbSet.As<IQueryable<LibreriaMaterial>>().Setup(x => x.Provider).Returns(new AsyncQueryProvider<LibreriaMaterial>(datarPrueba.Provider));
+
+            var contexto = new Mock<ContextoLibreria>();
+            contexto.Setup(x => x.LibreriaMaterial).Returns(dbSet.Object);
+            return contexto;
+        }
+
+        [Fact]
+        public async void GetLibroPorId()
+        {
+            var mockContexto = CrearContexto();
+            var mapConfig = new MapperConfiguration(cfg => {
+                cfg.AddProfile(new MappingTest());
+            });
+
+            var mapper = mapConfig.CreateMapper();
+
+            var request = new ConsultaFiltro.LibroUnico();
+            request.LibroId = Guid.Empty;
+
+            var manejador = new ConsultaFiltro.Manejador(mockContexto.Object, mapper);
+            var libro = await manejador.Handle(request, new System.Threading.CancellationToken());
+
+            Assert.NotNull(libro);
+            Assert.True(libro.LibreriaMaterialId == Guid.Empty);
+        }
+
+
+        [Fact]
+        public async void GetLibros() 
+        {
+            //System.Diagnostics.Debugger.Launch();
+
+            var mockContexto = CrearContexto();
+            var mapConfig = new MapperConfiguration(cfg => {
+                cfg.AddProfile(new MappingTest());
+                });
+
+            var mapper = mapConfig.CreateMapper();
+
+            Consulta.Manejador manejador = new Consulta.Manejador(mockContexto.Object, mapper);
+
+            Consulta.Ejecuta request = new Consulta.Ejecuta();
+
+            var lista = await manejador.Handle(request, new System.Threading.CancellationToken());
+
+            Assert.True(lista.Any());
+
+        }
+
+        [Fact]
+        public async void GuardarLibro()
+        {
+            System.Diagnostics.Debugger.Launch();
+
+            var options = new DbContextOptionsBuilder<ContextoLibreria>().UseInMemoryDatabase(databaseName: "BaseDatosLibro").Options;
+
+            var contexto = new ContextoLibreria(options);
+
+            var request = new Nuevo.Ejecuta();
+            request.Titulo = "Libro de MicroService";
+            request.AutorLibro = Guid.Empty;
+            request.FechaPublicacion = DateTime.Now;
+
+            var manejador = new Nuevo.Manejador(contexto);
+            var libro = await manejador.Handle(request, new System.Threading.CancellationToken());
+
+            Assert.True(libro != null);
+        }
+    }
+}
