7127e7a2397154aaf5b99bcd9d4521df32d29d3e
Cristián Lara <cristian.lara.n@gmail.com>
2023-11-16 11:59:17 -0300

    Implementación de envío de email

---
+++ TiendaServicios.Mensajeria.Email/SendGridLibreria/Interface/ISendGridEnviar.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TiendaServicios.Mensajeria.Email.SendGridLibreria.Modelo;
+
+namespace TiendaServicios.Mensajeria.Email.SendGridLibreria.Interface
+{
+    public interface ISendGridEnviar
+    {
+        Task<(bool resultado, string errorMessage)> EnviarEmail(SendGridData data);
+    }
+}
---
+++ TiendaServicios.Mensajeria.Email/SendGridLibreria/Modelo/SendGridData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TiendaServicios.Mensajeria.Email.SendGridLibreria.Modelo
+{
+    public class SendGridData
+    {
+        public string SendGridAPIKey { get; set; }
+        public string EmailDestinatario { get; set; }
+        public string NombreDestinatario { get; set; }
+        public string Titulo { get; set; }
+        public string Contenido { get; set; }
+    }
+}
